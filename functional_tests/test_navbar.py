197bc6e3ace654639eadb254a8ff967c4564a7a1
Matthew Blacklock <matthewblacklock@gmail.com>
2018-08-09 16:04:10 +0100

    navbar FT

---
+++ functional_tests/test_navbar.py
@@ -0,0 +1,40 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium import webdriver
+from unittest import skip
+
+import time
+
+from functional_tests.base import FunctionalTest
+
+class NavbarTest(FunctionalTest):
+
+    def test_navbar_login_works(self):
+        # Edith has heard about a cool new online free bets app. She goes
+        # to check out its homepage
+        self.browser.get(self.live_server_url)
+
+        # She starts a new summary and notices the navbar
+        self.browser.find_element_by_class_name('new_summary').click()
+        navbar = self.browser.find_element_by_class_name('navbar')
+
+        # There is a login form
+        login = navbar.find_element_by_class_name('login')
+
+        # Edith enters her email and clicks login
+        login.find_element_by_css_selector('input').clear()
+        login.find_element_by_css_selector('input').send_keys('edith@example.com')
+        login.find_element_by_css_selector('input').send_keys(Keys.ENTER)
+
+        # A message appears telling her an email has been sent
+        self.wait_for(lambda: self.assertIn(
+            'Check your email',
+            self.browser.find_element_by_tag_name('body').text
+        ))
+
+        # Satisfied, she goes back to sleep
+
+        
+
+       
+
