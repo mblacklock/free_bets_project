87edacf59341c7f8247906a469d2bb2f1a5f05b9
Matthew Blacklock <matthewblacklock@gmail.com>
2018-08-10 13:28:17 +0100

    user account can access admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,64 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from .models import User
 
 # Register your models here.
+
+class UserCreationForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user
+    """
+
+    class Meta:
+        model = User
+        fields = ('email', 'is_active','is_superuser','is_staff')
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email',)}),
+        ('Permissions', {'fields': ('is_active', 'is_superuser','is_staff')}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email',)}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+# Now register the new UserAdmin...
+admin.site.register(User, UserAdmin)
+# ... and, since we're not using Django's built-in permissions,
+# unregister the Group model from admin.
+admin.site.unregister(Group)
--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 import uuid
 
 # Create your models here.
 
-class User(models.Model):
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(primary_key=True)
 
     REQUIRED_FIELDS = []
     USERNAME_FIELD = 'email'
     is_anonymous = False
     is_authenticated = True
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    password = models.CharField(default='a', max_length=28)
     
 class Token(models.Model):
     email = models.EmailField()
