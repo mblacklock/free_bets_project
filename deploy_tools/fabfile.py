caec4287b04729a0a3ec9ae8266289f3bedb2c2e
Matthew Blacklock <matthewblacklock@gmail.com>
2018-08-16 22:42:54 +0100

    finished fabric

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -8,26 +8,30 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 REPO_URL = 'https://github.com/mblacklock/free_bets_project.git'
 
-def practice():
-    print(BASE_DIR)
-    read_dotenv(os.path.join(BASE_DIR, '.env'))
-    print(os.environ['EMAIL_PASSWORD'])
-
-def deploy():
+def deploy(VERSION):
+    if VERSION == 'dev':
+        print('Deploying development site...')
+        SITENAME = 'mblacklock.pythonanywhere.com'
+        WSGI_FILE = 'mblacklock_pythonanywhere_com_wsgi.py'
+    elif VERSION == 'prod':
+        print('Deploying production site...')
+        SITENAME = 'mblacklock.pythonanywhere.com'
+        WSGI_FILE = 'mblacklock_pythonanywhere_com_wsgi.py'
+        
     read_dotenv(os.path.join(BASE_DIR, '.env'))
     
-    virtualenv = 'free-bets-project-dev'
-    virtualenv_path = '/home/mblacklock/.virtualenvs/'+virtualenv
-    site_folder = f'/home/{env.user}/free_bets_project_dv/'
+    virtualenv = 'free-bets-project-' + VERSION
+    virtualenv_path = '/home/mblacklock/.virtualenvs/' + virtualenv
+    site_folder = f'/home/mblacklock/free_bets_project_' + VERSION + '/'
     
     run(f'mkdir -p {site_folder}')  
     with cd(site_folder):  
         _get_latest_source()
         _update_virtualenv(virtualenv, virtualenv_path)
-        _create_or_update_dotenv()
+        _create_or_update_dotenv(SITENAME)
         #_update_static_files()
         _update_database(virtualenv, virtualenv_path)
-    run(f'touch /var/www/mblacklock_pythonanywhere_com_wsgi.py')
+    run(f'touch /var/www/' + WSGI_FILE)
 
 def _get_latest_source():
     if exists('.git'):  
@@ -42,10 +46,10 @@ def _update_virtualenv(virtualenv, virtualenv_path):
         run(f'mkvirtualenv '+virtualenv+' --python=/usr/bin/python3.6')
     run(virtualenv_path + '/bin/pip install -r requirements.txt') 
 
-def _create_or_update_dotenv():
+def _create_or_update_dotenv(SITENAME):
     append('.env', 'DJANGO_DEBUG_FALSE=Y')  
-    append('.env', 'SITENAME=mblacklock.pythonanywhere.com')
-    append('.env', 'EMAIL_PASSWORD='+os.environ['EMAIL_PASSWORD'])  
+    append('.env', 'SITENAME=' + SITENAME)
+    append('.env', 'EMAIL_PASSWORD=' + os.environ['EMAIL_PASSWORD'])  
     current_contents = run('cat .env')  
     if 'DJANGO_SECRET_KEY' not in current_contents:  
         new_secret = ''.join(random.SystemRandom().choices(  
